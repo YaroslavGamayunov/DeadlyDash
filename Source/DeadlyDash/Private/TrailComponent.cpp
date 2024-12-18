863cdc2b4163c3390472f5d67bfff224de88b5e7
YaroslavGamayunov <yaroslav.gamayunov@gmail.com>
2024-12-18 11:51:40 +0300

    DEADLYDASH-17: Improved trail component

--- Source/DeadlyDash/Private/TrailComponent.cpp
+++ Source/DeadlyDash/Private/TrailComponent.cpp
@@ -1,57 +1,56 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "TrailComponent.h"
-#include "Components/SplineComponent.h"
-#include "Engine/World.h"
-#include "Logging/StructuredLog.h"
+#include "ProceduralMeshComponent.h"
 #include "Materials/MaterialInstanceDynamic.h"
 
 UTrailComponent::UTrailComponent()
 {
     PrimaryComponentTick.bCanEverTick = true;
-    
-    // Create spline component
-    TrailSpline = CreateDefaultSubobject<USplineComponent>(TEXT("TrailSpline"));
-    TrailSpline->SetupAttachment(this);
+
+    // Создаем компонент процедурного меша и регистрируем его
+    TrailMeshComponent = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("TrailMeshComponent"));
+
+    // Не прикрепляем меш к игроку, чтобы он рисовался относительно уровня
+    TrailMeshComponent->SetupAttachment(nullptr);
+
+    // Устанавливаем положение меша в нуль, чтобы он был в мировых координатах
+    TrailMeshComponent->SetWorldLocation(FVector::ZeroVector);
+
+    TrailMeshComponent->SetMobility(EComponentMobility::Movable);
+
+    bIsTrailActive = false;
 }
 
 void UTrailComponent::BeginPlay()
 {
     Super::BeginPlay();
 
+    // Убеждаемся, что компонент меша зарегистрирован
+    if (!TrailMeshComponent->IsRegistered())
+    {
+        TrailMeshComponent->RegisterComponent();
+    }
+
+    // Создаем динамический экземпляр материала
     if (TrailMaterial)
     {
         TrailMaterialInstance = UMaterialInstanceDynamic::Create(TrailMaterial, this);
+        TrailMeshComponent->SetMaterial(0, TrailMaterialInstance);
     }
-
-    bIsTrailActive = false;
-}
-
-void UTrailComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-    if (!bIsTrailActive) return;
-
-    // Get current location
-    FVector CurrentLocation = GetComponentLocation();
-
-    // Add new point if needed
-    if (TrailPoints.Num() == 0 || 
-        FVector::Distance(TrailPoints.Last(), CurrentLocation) > MinDistanceBetweenPoints)
-    {
-        TrailPoints.Add(CurrentLocation);
-        PointTimestamps.Add(GetWorld()->GetTimeSeconds());
-        UpdateSpline();
-    }
-
-    CleanupOldPoints();
 }
 
 void UTrailComponent::StartTrail()
 {
     bIsTrailActive = true;
+
+    // Очищаем предыдущие точки и меш
+    ClearTrail();
+
+    // Добавляем начальную точку
+    FVector CurrentLocation = GetComponentLocation();
+    TrailPoints.Add(CurrentLocation);
+    PointTimestamps.Add(GetWorld()->GetTimeSeconds());
+
+    UpdateTrailMeshes();
 }
 
 void UTrailComponent::StopTrail()
@@ -63,89 +62,213 @@ void UTrailComponent::ClearTrail()
 {
     TrailPoints.Empty();
     PointTimestamps.Empty();
-    
-    // Clean up spline meshes
-    for (auto* Mesh : SplineMeshes)
+
+    // Очищаем меш
+    if (TrailMeshComponent)
     {
-        if (Mesh)
-        {
-            Mesh->DestroyComponent();
-        }
+        TrailMeshComponent->ClearAllMeshSections();
     }
-    SplineMeshes.Empty();
-    
-    TrailSpline->ClearSplinePoints();
 }
 
 void UTrailComponent::SetTrailColor(FLinearColor NewColor)
 {
     if (TrailMaterialInstance)
     {
-        TrailMaterialInstance->SetVectorParameterValue("Color", NewColor);
+        TrailMaterialInstance->SetVectorParameterValue(FName("Color"), NewColor);
     }
 }
 
-void UTrailComponent::UpdateSpline()
+void UTrailComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-    TrailSpline->ClearSplinePoints();
-    
-    for (int32 i = 0; i < TrailPoints.Num(); i++)
-    {
-        TrailSpline->AddSplinePoint(TrailPoints[i], ESplineCoordinateSpace::World);
-    }
-    
-    UpdateTrailMeshes();
-}
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-void UTrailComponent::UpdateTrailMeshes()
-{
-    // Clean up old meshes
-    for (auto* Mesh : SplineMeshes)
+    if (bIsTrailActive)
     {
-        if (Mesh)
+        CleanupOldPoints();
+
+        FVector CurrentLocation = GetComponentLocation();
+
+        if (TrailPoints.Num() == 0 || FVector::Dist(CurrentLocation, TrailPoints.Last()) >= MinDistanceBetweenPoints)
         {
-            Mesh->DestroyComponent();
+            // Добавляем новую точку
+            TrailPoints.Add(CurrentLocation);
+            PointTimestamps.Add(GetWorld()->GetTimeSeconds());
+
+            UpdateTrailMeshes();
         }
-    }
-    SplineMeshes.Empty();
-
-    UE_LOGFMT(LogCore, Warning, "Loading `{Name}` failed with error {Error}", Package->GetName(),  ErrorCode);
-
-    // Create new meshes
-    for (int32 i = 0; i < TrailPoints.Num() - 1; i++)
-    {
-        USplineMeshComponent* SplineMesh = NewObject<USplineMeshComponent>(this);
-        SplineMesh->SetMobility(EComponentMobility::Movable);
-        SplineMesh->AttachToComponent(TrailSpline, FAttachmentTransformRules::KeepRelativeTransform);
-        SplineMesh->SetStaticMesh(TrailMesh);
-        SplineMesh->SetMaterial(0, TrailMaterialInstance);
-        
-        FVector StartPoint, StartTangent, EndPoint, EndTangent;
-        TrailSpline->GetLocationAndTangentAtSplinePoint(i, StartPoint, StartTangent, ESplineCoordinateSpace::World);
-        TrailSpline->GetLocationAndTangentAtSplinePoint(i + 1, EndPoint, EndTangent, ESplineCoordinateSpace::World);
-        
-        SplineMesh->SetStartAndEnd(StartPoint, StartTangent, EndPoint, EndTangent, true);
-        SplineMesh->SetStartScale(FVector2D(TrailWidth, TrailWidth));
-        SplineMesh->SetEndScale(FVector2D(TrailWidth, TrailWidth));
-        
-        SplineMesh->RegisterComponent();
-        SplineMeshes.Add(SplineMesh);
     }
 }
 
 void UTrailComponent::CleanupOldPoints()
 {
     float CurrentTime = GetWorld()->GetTimeSeconds();
-    
-    while (PointTimestamps.Num() > 0 && 
-           (CurrentTime - PointTimestamps[0]) > TrailLifetime)
+
+    // Удаляем точки, которые старше TrailLifetime
+    while (PointTimestamps.Num() > 0 && (CurrentTime - PointTimestamps[0]) > TrailLifetime)
     {
-        PointTimestamps.RemoveAt(0);
         TrailPoints.RemoveAt(0);
+        PointTimestamps.RemoveAt(0);
+
+        // Перестраиваем меш после удаления старых точек
+        UpdateTrailMeshes();
     }
-    
-    if (TrailPoints.Num() > 0)
+}
+
+// todo check performance (because mesh is recreated every tick)
+void UTrailComponent::UpdateTrailMeshes()
+{
+    if (TrailPoints.Num() < 2 || !TrailMeshComponent)
     {
-        UpdateSpline();
+        return;
+    }
+    TArray<FVector> Vertices;
+    TArray<int32> Triangles;
+    TArray<FVector> Normals;
+    TArray<FVector2D> UVs;
+    TArray<FProcMeshTangent> Tangents;
+
+    // Половина ширины и высоты пути
+    float TrailWidth = Width * 0.5f;
+    float TrailHeight = Height * 0.5f;
+
+    int32 NumPoints = TrailPoints.Num();
+
+    for (int32 i = 0; i < NumPoints; ++i)
+    {
+        FVector CurrPoint = TrailPoints[i];
+
+        FVector ForwardDir;
+        if (i < NumPoints - 1)
+        {
+            ForwardDir = (TrailPoints[i + 1] - CurrPoint).GetSafeNormal();
+        }
+        else if (i > 0)
+        {
+            ForwardDir = (CurrPoint - TrailPoints[i - 1]).GetSafeNormal();
+        }
+        else
+        {
+            ForwardDir = FVector::ForwardVector;
+        }
+
+        FVector UpDir = FVector::UpVector; // Если "вверх" по оси Z
+        FVector RightDir = FVector::CrossProduct(ForwardDir, UpDir).GetSafeNormal();
+
+        // Четыре угла с учётом ширины и высоты
+        FVector BottomLeft = CurrPoint - RightDir * TrailWidth - UpDir * TrailHeight;
+        FVector BottomRight = CurrPoint + RightDir * TrailWidth - UpDir * TrailHeight;
+        FVector TopLeft = CurrPoint - RightDir * TrailWidth + UpDir * TrailHeight;
+        FVector TopRight = CurrPoint + RightDir * TrailWidth + UpDir * TrailHeight;
+
+        // Добавляем вершины
+        Vertices.Add(BottomLeft);   // Индекс: i*4 + 0
+        Vertices.Add(BottomRight);  // Индекс: i*4 + 1
+        Vertices.Add(TopLeft);      // Индекс: i*4 + 2
+        Vertices.Add(TopRight);     // Индекс: i*4 + 3
+
+        // Добавляем нормали для каждой вершины
+        // Для упрощения можно использовать одну и ту же нормаль, но для правильного освещения рекомендуется вычислить нормали для каждой грани
+        Normals.Add(-ForwardDir); // Нижние вершины
+        Normals.Add(-ForwardDir);
+        Normals.Add(ForwardDir);  // Верхние вершины
+        Normals.Add(ForwardDir);
+
+        // UV координаты
+        float UCoord = i / (float)(NumPoints - 1);
+
+        UVs.Add(FVector2D(UCoord, 0.0f)); // BottomLeft
+        UVs.Add(FVector2D(UCoord, 1.0f)); // BottomRight
+        UVs.Add(FVector2D(UCoord, 0.0f)); // TopLeft
+        UVs.Add(FVector2D(UCoord, 1.0f)); // TopRight
+
+        // Тангенты
+        Tangents.Add(FProcMeshTangent(RightDir, false));
+        Tangents.Add(FProcMeshTangent(RightDir, false));
+        Tangents.Add(FProcMeshTangent(RightDir, false));
+        Tangents.Add(FProcMeshTangent(RightDir, false));
+    }
+
+    // Создание треугольников для каждой секции пути
+    for (int32 i = 0; i < NumPoints - 1; ++i)
+    {
+        int32 IndexBase = i * 4;
+
+        // Индексы текущей и следующей точки
+        int32 BL0 = IndexBase + 0; // BottomLeft текущий
+        int32 BR0 = IndexBase + 1; // BottomRight текущий
+        int32 TL0 = IndexBase + 2; // TopLeft текущий
+        int32 TR0 = IndexBase + 3; // TopRight текущий
+
+        int32 BL1 = IndexBase + 4; // BottomLeft следующий
+        int32 BR1 = IndexBase + 5; // BottomRight следующий
+        int32 TL1 = IndexBase + 6; // TopLeft следующий
+        int32 TR1 = IndexBase + 7; // TopRight следующий
+
+        // Верхняя грань
+        Triangles.Add(TL0);
+        Triangles.Add(TR1);
+        Triangles.Add(TR0);
+
+        Triangles.Add(TL0);
+        Triangles.Add(TL1);
+        Triangles.Add(TR1);
+
+        // Нижняя грань
+        Triangles.Add(BL0);
+        Triangles.Add(BR0);
+        Triangles.Add(BR1);
+
+        Triangles.Add(BL0);
+        Triangles.Add(BR1);
+        Triangles.Add(BL1);
+
+        // Левая грань
+        Triangles.Add(BL0);
+        Triangles.Add(TL1);
+        Triangles.Add(TL0);
+
+        Triangles.Add(BL0);
+        Triangles.Add(BL1);
+        Triangles.Add(TL1);
+
+        // Правая грань
+        Triangles.Add(BR0);
+        Triangles.Add(TR0);
+        Triangles.Add(TR1);
+
+        Triangles.Add(BR0);
+        Triangles.Add(TR1);
+        Triangles.Add(BR1);
+
+        // Передняя грань (по направлению движения)
+        Triangles.Add(TL0);
+        Triangles.Add(TR0);
+        Triangles.Add(BR0);
+
+        Triangles.Add(TL0);
+        Triangles.Add(BR0);
+        Triangles.Add(BL0);
+
+        // Задняя грань
+        Triangles.Add(TL1);
+        Triangles.Add(BR1);
+        Triangles.Add(TR1);
+
+        Triangles.Add(TL1);
+        Triangles.Add(BL1);
+        Triangles.Add(BR1);
+    }
+
+    // Очищаем предыдущие секции меша
+    TrailMeshComponent->ClearAllMeshSections();
+
+    // Создаем новую секцию меша
+    TrailMeshComponent->CreateMeshSection(0, Vertices, Triangles, Normals, UVs, TArray<FColor>(), Tangents, true);
+    TrailMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision); // todo check
+
+    // Устанавливаем материал
+    if (TrailMaterialInstance)
+    {
+        TrailMeshComponent->SetMaterial(0, TrailMaterialInstance);
     }
 }
--- Source/DeadlyDash/Public/TrailComponent.h
+++ Source/DeadlyDash/Public/TrailComponent.h
@@ -1,16 +1,13 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ProceduralMeshComponent.h"
 #include "Components/SceneComponent.h"
-#include "Components/SplineComponent.h"
-#include "Components/SplineMeshComponent.h"
 #include "Engine/World.h"
 #include "Materials/MaterialInstanceDynamic.h"
 #include "TrailComponent.generated.h"
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class DEADLYDASH_API UTrailComponent : public USceneComponent
 {
 	GENERATED_BODY()
@@ -18,21 +15,25 @@ class DEADLYDASH_API UTrailComponent : public USceneComponent
 public:
 	UTrailComponent();
 
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
 
 	// Configuration properties
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trail")
-	float TrailLifetime = 2.0f;
+	float TrailLifetime = 5.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trail")
-	float MinDistanceBetweenPoints = 10.0f;
+	float MinDistanceBetweenPoints = 100.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trail")
 	UMaterialInterface* TrailMaterial;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trail")
-	float TrailWidth = 10.0f;
+	float Width = 100.0f; // Ширина полосы
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trail")
+	float Height = 100.0f; // Высота полосы
 
 	// Control functions
 	UFUNCTION(BlueprintCallable, Category = "Trail")
@@ -49,12 +50,6 @@ public:
 
 protected:
 	UPROPERTY()
-	USplineComponent* TrailSpline;
-
-	UPROPERTY()
-	TArray<USplineMeshComponent*> SplineMeshes;
-
-	UPROPERTY()
 	TArray<FVector> TrailPoints;
 
 	UPROPERTY()
@@ -63,12 +58,11 @@ protected:
 	UPROPERTY()
 	UMaterialInstanceDynamic* TrailMaterialInstance;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trail")
-	UStaticMesh* TrailMesh;
+	UPROPERTY()
+	UProceduralMeshComponent* TrailMeshComponent;
 
 	bool bIsTrailActive;
 
-	void UpdateSpline();
 	void UpdateTrailMeshes();
 	void CleanupOldPoints();
 };
